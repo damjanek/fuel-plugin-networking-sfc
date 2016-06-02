e5acb65b1f23f54c2cb37797b4803f4a7675ddb7
Maciej Relewicz <mrelewicz@mirantis.com>
2016-06-02 15:08:06 +0200

    Manifest optiminimalization, fixes

--- deployment_scripts/puppet/manifests/networking-sfc-controller.pp
+++ deployment_scripts/puppet/manifests/networking-sfc-controller.pp
@@ -1,4 +1,4 @@
-#    Copyright 2016 Mirantis, Inc.
+opyright 2016 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -17,16 +17,36 @@ notice('MODULAR: networking-sfc/networking-sfc-controller.pp')
 $use_neutron = hiera('use_neutron', false)
 
 if $use_neutron {
+  include ::neutron::params
 
   $primary_controller = hiera('primary_controller')
+  $node_name = hiera('node_name')
+  $neutron_config = hiera_hash('neutron_config')
+  $neutron_server_enable = pick($neutron_config['neutron_server_enable'], true)
+  $neutron_nodes = hiera_hash('neutron_nodes')
 
-  service {'neutron-openvswitch-agent':
-    ensure    => running,
-    enable    => true,
-    hasstatus => true,
+  $default_service_plugins = [
+    'neutron.services.l3_router.l3_router_plugin.L3RouterPlugin',
+    'neutron.services.metering.metering_plugin.MeteringPlugin',
+  ]
+
+  if $node_name in keys($neutron_nodes) {
+    if $neutron_server_enable {
+      $service_ensure = 'running'
+    } else {
+      $service_ensure = 'stopped'
+    }
+    service { 'neutron-server':
+      name       => $::neutron::params::server_service,
+      enable     => $neutron_server_enable,
+      ensure     => $service_ensure,
+      hasstatus  => true,
+      hasrestart => true,
+      tag        => 'neutron-service',
+    }
   }
 
-  service {'neutron-server':
+  service {'neutron-openvswitch-agent':
     ensure    => running,
     enable    => true,
     hasstatus => true,
@@ -38,21 +58,23 @@ if $use_neutron {
 
   if $primary_controller {
     $extra_params = '--config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini --subproject networking-sfc'
-    include ::neutron::db::sync
-    notify{"Schema upgrade for SFC": } ~> Exec['neutron-db-sync'] -> Exec ['Modify neutron.conf'] -> Exec ['Modify neutron-openvswitch-agent.conf']
+
+    class { '::neutron::db::sync':
+      extra_params => $extra_params,
+    }
+   notify{"Schema upgrade for SFC": } ~> Exec['neutron-db-sync']
   }
 
-  # TODO: replace it with something less ugly
-  exec { 'Modify neutron.conf':
-    command => "sed -i '/^service_plugins/ s/$/,networking_sfc.services.flowclassifier.plugin.FlowClassifierPlugin,networking_sfc.services.sfc.plugin.SfcPlugin/' /etc/neutron/neutron.conf && echo '\n[sfc]\ndrivers = ovs\n' >> /etc/neutron/neutron.conf && touch /usr/local/sfc_configured",
-    path    => '/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin',
-    creates => '/usr/local/sfc_configured',
+  neutron_config { 'DEFAULT/service_plugins': value => join($default_service_plugins, 'networking_sfc.services.flowclassifier.plugin.FlowClassifierPlugin,networking_sfc.services.sfc.plugin.SfcPlugin,') } ->
+
+  file_line { 'Add OSV section to neutron.conf':
+    path => '/etc/neutron/neutron.conf',
+    line => "\n[sfc]\ndrivers = ovs\n",
   } ->
-  # TODO: same here
+
   exec { 'Modify neutron-openvswitch-agent.conf':
-    command => "sed -i 's|/usr/bin|/usr/local/bin|g' /etc/init/neutron-openvswitch-agent.conf && touch /usr/local/sfc_configured2",
+    command => "sed -i 's|/usr/bin|/usr/local/bin|g' /etc/init/neutron-openvswitch-agent.conf",
     path    => '/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin',
-    creates => '/usr/local/sfc_configured2',
     notify  => [Service['neutron-openvswitch-agent'],Service['neutron-server']],
   }
 }
